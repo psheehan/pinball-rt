dd5cd9076a28c84b73fa0d94cddb440d6068190b
Patrick Sheehan <psheehan287@gmail.com>
2025-10-02 21:57:23 +0000

    Update tests to (hopefully) work now.

--- pinballrt/tests/test_E2E.py
+++ pinballrt/tests/test_E2E.py
@@ -7,6 +7,7 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+import torch
 import os
 
 import pytest
@@ -22,6 +23,11 @@ def test_E2E(grid_class, grid_kwargs, return_vals=False):
     """
     Test the end-to-end functionality of the UniformCartesianGrid model running all the way through.
     """
+    # Set the random seeds so that the result stays constant.
+
+    torch.manual_seed(1)
+    np.random.seed(1)
+
     # Set up the dust.
 
     d = os.path.join(os.path.dirname(__file__), "data/yso.dst")
@@ -41,27 +47,25 @@ def test_E2E(grid_class, grid_kwargs, return_vals=False):
 
     model.thermal_mc(nphotons=1000000, use_ml_step=False)
 
-    image = model.make_image(npix=256, pixel_size=0.1*u.arcsec, lam=np.array([1., 1000.])*u.micron, incl=45.*u.degree, pa=45.*u.degree, distance=1.*u.pc)
+    image = model.make_image(npix=256, pixel_size=0.2*u.arcsec, lam=np.array([1., 1000.])*u.micron, incl=45.*u.degree, pa=45.*u.degree, distance=1.*u.pc)
 
     # Do the checks.
 
     if not return_vals:
         # Load the comparison data.
 
-        temperature_mean = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_temperature.npz"))['temperature_mean']
-        temperature_std = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_temperature.npz"))['temperature_std']
-        scattering_mean = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_scattering.npz"))['scattering_mean']
-        scattering_std = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_scattering.npz"))['scattering_std']
+        temperature = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_temperature.npz"))['temperature']
+        scattering = np.load(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_scattering.npz"))['scattering']
         base_image = xr.open_dataset(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_image.nc"))
 
-        temperature_diff = (model.grid.grid.temperature.numpy() - temperature_mean) / temperature_std
-        assert np.all(np.abs(temperature_diff) <= 3.), f"Temperature difference exceeds tolerance: {np.max(np.abs(temperature_diff))}"
+        temperature_diff = np.abs(model.grid.grid.temperature.numpy() - temperature) / temperature
+        assert np.all(temperature_diff < 1e-4), f"Temperature difference exceeds tolerance: {np.max(temperature_diff)}"
 
-        scattering_diff = (model.grid.scattering.numpy() - scattering_mean) / np.where(scattering_std > 0, scattering_std, 1.0)
-        assert np.all(np.abs(scattering_diff) < 3.), f"Scattering difference exceeds tolerance: {np.max(np.abs(scattering_diff))}"
+        scattering_diff = np.abs(model.grid.scattering.numpy() - scattering) / np.where(np.maximum(model.grid.scattering.numpy(), scattering) > 0, np.maximum(model.grid.scattering.numpy(), scattering), 1.0)
+        assert np.all(scattering_diff < 1e-4), f"Scattering difference exceeds tolerance: {np.max(scattering_diff)}"
 
-        image_diff = (image.intensity - base_image.intensity) / np.where(base_image.unc > 0, base_image.unc, 1.0)
-        assert np.all(np.abs(image_diff) < 3.), f"Image difference exceeds tolerance: {np.max(np.abs(image_diff))}"
+        image_diff = np.abs(image.intensity - base_image.intensity) / np.where(np.maximum(image.intensity, base_image.intensity) > 0, np.maximum(image.intensity, base_image.intensity), 1.0)
+        assert np.all(image_diff < 1e-4), f"Image difference exceeds tolerance: {np.max(image_diff)}"
     else:
         return model.grid.grid.temperature.numpy(), model.grid.scattering.numpy(), image
 
@@ -75,21 +79,8 @@ def update_test(test, grid_class):
     if not found:
         raise ValueError(f"Grid class {grid_class} not found in test data. Please add it to the test_data list in test_E2E.py.")
     
-    result = [test(grid_class, data[1], return_vals=True) for i in range(5)]
+    temperature, scattering, image = test(grid_class, data[1], return_vals=True)
 
-    temperature_list = [r[0] for r in result]
-    scattering_list = [r[1] for r in result]
-    image_list = [r[2] for r in result]
-
-    temperature_mean = np.mean(temperature_list, axis=0)
-    temperature_std = np.std(temperature_list, axis=0)
-    scattering_mean = np.mean(scattering_list, axis=0)
-    scattering_std = np.std(scattering_list, axis=0)
-    image_mean = xr.concat(image_list, dim="iteration").mean(dim="iteration")
-    image_std = xr.concat(image_list, dim="iteration").std(dim="iteration")
-
-    image = image_mean.assign(unc=image_std.intensity)
-
-    np.savez(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_temperature.npz"), temperature_mean=temperature_mean, temperature_std=temperature_std)
-    np.savez(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_scattering.npz"), scattering_mean=scattering_mean, scattering_std=scattering_std)
+    np.savez(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_temperature.npz"), temperature=temperature)
+    np.savez(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_scattering.npz"), scattering=scattering)
     image.to_netcdf(os.path.join(os.path.dirname(__file__), f"data/{grid_class.__name__}_E2E_image.nc"))
