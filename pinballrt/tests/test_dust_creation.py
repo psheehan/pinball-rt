0a885f2ca6de66de85ee441bd420785734e796cf
Patrick Sheehan <psheehan287@gmail.com>
2026-03-10 18:32:28 -0500

    Change model in test_dust_creation.py from 'ksca' to 'pmo'

--- pinballrt/tests/test_dust_creation.py
+++ pinballrt/tests/test_dust_creation.py
@@ -27,7 +27,7 @@ def test_learning():
 
     n_samples = 1000
 
-    for model in ["ksca"]:
+    for model in ["pmo"]:
         print('*****************************')
         print(f'{model}')
         print('*****************************')
